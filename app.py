a24306e42eae8112013e266211d561f2a5301b32
HIMANSHU RANA <56638837+Bharat-RanaHimanshu@users.noreply.github.com>
2026-02-16 21:27:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ if st.session_state.vector_db:
             search_type="mmr",
             search_kwargs={
                 "k": 8,                # Maximum number of chunks to ever show
-                "score_threshold": 0.25, # Minimum "quality" score to be included
+                "score_threshold": 0.40, # Minimum "quality" score to be included
                 'fetch_k': 20
             }
         )
@@ -171,6 +171,7 @@ if st.session_state.vector_db:
 else:
 
     st.info("Upload one or multiple PDFs to begin.")
+
 
 
 
